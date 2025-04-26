913f49d3005511e7a84d84e2788646d9b945939a
charles-m-knox <code@charlesmknox.com>
2025-04-26 14:27:00 -0700

    update text story description

--- src/stories/TextCopy.stories.tsx
+++ src/stories/TextCopy.stories.tsx
@@ -19,7 +19,7 @@ Default.args = {
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tristique purus at rutrum gravida. Mauris tempor, enim in laoreet feugiat, magna nisl tincidunt orci, a hendrerit tortor metus iaculis urna. Mauris tincidunt pulvinar dignissim. Aliquam euismod, orci a molestie faucibus, magna lorem consequat lorem, sit amet mollis leo tortor nec.',
   label: 'Copy text label',
   descriptionHeading: 'Text Copy',
-  description: 'This is description',
+  description: 'This is the description',
   copyButtonId: 'f10537bc-dcd1-4f22-83e5-01724a26d847',
   tooltipId: '7983131f-3491-480a-a103-80fa3a84d804',
 }
